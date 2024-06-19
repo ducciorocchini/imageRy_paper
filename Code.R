be3c9b17d4c0dc823708f096840c37da2b4b14f5
Duccio Rocchini <ducciorocchini@gmail.com>
2024-06-19 18:07:22 +0200

    Create Code.R

---
+++ Code.R
@@ -0,0 +1,7 @@
+
+
+# Classification: final plot
+library(patchwork)
+p1 <- ggplot(tabout, aes(x=class, y=p1992, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
+p2 <- ggplot(tabout, aes(x=class, y=p2006, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
+p1 + p2
